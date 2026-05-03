4d36fe51087e33852442dbc8df320f0c55e30dd3
CioF <coder.walidhasan@gmail.com>
2026-05-03 13:17:10 +0600

    Solve task 1, Temp to Celcius

---
+++ script.js
@@ -0,0 +1,19 @@
+// Practice Problems
+
+
+
+// Task-1: Write a function to convert temperature from Celsius to Fahrenheit.
+
+// Solution of task-1:
+
+// function celsiusToFahrenheit(celsius) {
+//   fahrenheit = celsius * 1.8 + 32;
+//   return fahrenheit;
+// }
+
+// console.log(
+//   `${celsius} celsius is equal to ${celsiusToFahrenheit(10)} fahrenheit.`,
+// );
+
+
+// 
